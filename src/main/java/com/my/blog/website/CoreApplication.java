f18ec72d0b9d6b3120897f77180ea1d282d653aa
startoffly <15757871020@163.com>
2018-12-20 10:11:04 +0800

    测试提交

--- src/main/java/com/my/blog/website/CoreApplication.java
+++ src/main/java/com/my/blog/website/CoreApplication.java
@@ -47,7 +47,6 @@ public class CoreApplication extends SpringBootServletInitializer
         return new DataSourceTransactionManager(dataSource());
     }
 
-
     public static void main(String[] args) {
         SpringApplication.run(CoreApplication.class, args);
     }
